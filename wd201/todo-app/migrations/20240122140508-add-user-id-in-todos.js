5410dd7f69aca60d20e4d4197e43d84c1523951e
lohitha-06 <savvanalohitha@gmail.com>
2025-06-17 11:06:00 +0530

    initial

---
+++ wd201/todo-app/migrations/20240122140508-add-user-id-in-todos.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    const tableInfo = await queryInterface.describeTable('Todos');
+    if (!tableInfo.userId) {
+      await queryInterface.addColumn('Todos', 'userId', {
+        type: Sequelize.DataTypes.INTEGER
+      });
+    }
+
+    await queryInterface.addConstraint('Todos', {
+      fields: ['userId'],
+      type: 'foreign key',
+      references: {
+        table: 'Users',
+        field: 'id'
+      }
+    })
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Todos', 'userId')
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+};
